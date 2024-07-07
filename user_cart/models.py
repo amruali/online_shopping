f21d0d49d3f060a1de543c88cbf9854ab88721a8
Amr Ali <amr.ali@damen.com.eg>
2024-07-07 21:24:36 +0300

    :hammer: Edited models.py - Edited cart-item added status to be default to 0 - added property function total price

--- user_cart/models.py
+++ user_cart/models.py
@@ -16,6 +16,11 @@ class CartItem(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
+    status = models.IntegerField(default=0)  # 0 for not checked out, 1 for checked out
+
+    @property
+    def total_price(self):
+        return self.quantity * self.product.price
 
     def __str__(self):
         return f"{self.quantity} x {self.product.name}"
