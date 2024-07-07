96401d92be4e59922df865a43946fd6b84f81d61
Amr Ali <amr.ali@damen.com.eg>
2024-07-07 21:27:05 +0300

    :truck: Edietd urls.py - Edited Added route for checkout

--- user_cart/urls.py
+++ user_cart/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 # from rest_framework_simplejwt.views import TokenObtainPairView #, TokenRefreshView
 # from . import views
 
-from .views import ProductListView, CartItemListView, CartItemDetailView, UserCreateView, MyTokenObtainPairView, hello_world
+from .views import ProductListView, CartItemListView, CartItemDetailView, UserCreateView, MyTokenObtainPairView, CheckoutView, hello_world
 
 from rest_framework_simplejwt.views import TokenRefreshView
 
@@ -16,6 +16,7 @@ urlpatterns = [
     path('products/<int:pk>/', ProductListView.as_view(), name='product-detail'),
     path('cart/', CartItemListView.as_view(), name='cart-list'),
     path('cart/<int:pk>/', CartItemDetailView.as_view(), name='cart-detail'),
+    path('checkout/', CheckoutView.as_view(), name='checkout'),
     path('register/', UserCreateView.as_view(), name='user_register'),
     # path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
