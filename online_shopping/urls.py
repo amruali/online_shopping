a09eed4e4450a04f57f6ac7031c5f279a942f2c5
Amr Ali <amr.ali@damen.com.eg>
2024-07-08 15:44:17 +0300

    :hammer: Edited urls.py - Added HomePage view

--- online_shopping/urls.py
+++ online_shopping/urls.py
@@ -15,11 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import render
+
+
+def HomePageView(request):
+    return render(request, 'index.html')
 
 
 urlpatterns = [
+    path('', HomePageView, name='index'),
     path('admin/', admin.site.urls),
     # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),    
     path('api/v1/', include('user_cart.urls')),
 ]
+
+
