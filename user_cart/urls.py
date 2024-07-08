9e39d2c32dab149c10b643ccf59bc54a29db7059
Amr Ali <amr.ali@damen.com.eg>
2024-07-08 14:40:02 +0300

    :truck: Edited user_cart/urls.py - replaced path with re_path for the sake of trailing the '/'

--- user_cart/urls.py
+++ user_cart/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 
 # from rest_framework_simplejwt.views import TokenObtainPairView #, TokenRefreshView
 # from . import views
@@ -9,16 +9,16 @@ from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     # test
-    path('hello/', hello_world, name='hello_world'),
+    re_path(r'hello/?$', hello_world, name='hello_world'),
 
     # project 
-    path('products/', ProductListView.as_view(), name='product-list'),
-    path('products/<int:pk>/', ProductListView.as_view(), name='product-detail'),
-    path('cart/', CartItemListView.as_view(), name='cart-list'),
-    path('cart/<int:pk>/', CartItemDetailView.as_view(), name='cart-detail'),
-    path('checkout/', CheckoutView.as_view(), name='checkout'),
-    path('register/', UserCreateView.as_view(), name='user_register'),
+    re_path(r'products/?$', ProductListView.as_view(), name='product-list'),
+    re_path(r'products/<int:pk>/?$', ProductListView.as_view(), name='product-detail'),
+    re_path(r'cart/?$', CartItemListView.as_view(), name='cart-list'),
+    re_path(r'cart/<int:pk>/?$', CartItemDetailView.as_view(), name='cart-detail'),
+    re_path(r'checkout/?$', CheckoutView.as_view(), name='checkout'),
+    re_path(r'register/?$', UserCreateView.as_view(), name='user_register'),
     # path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
+    re_path(r'token/?$', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    re_path(r'token/refresh/?$', TokenRefreshView.as_view(), name='token_refresh')
 ]
